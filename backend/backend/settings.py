850e262a855b8c8b71dc82b2eb7566b85dcc6e10
art_nix <bobrolevv@yandex.ru>
2022-01-27 09:51:28 +0700

    add djoser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'recipes',
     'users',
     'rest_framework',
-    # 'djoser',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': 'True',
+    'SERIALIZERS': {},
+}
+
+SIMPLE_JWT = {
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+   'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,37 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class userProfile(models.Model):
+#     email = models.EmailField(
+#         max_length=254,)
+#
+#     username = models.OneToOneField(
+#         User,
+#         max_length=150,
+#         on_delete=models.CASCADE,
+#         related_name="profile")
+#
+#     first_name = models.CharField(
+#         max_length=150,
+#         )
+#
+#     last_name = models.CharField(
+#         max_length=150,
+#         )
+#
+#     description = models.TextField(
+#         blank=True,
+#         null=True)
+#     location = models.CharField(
+#         max_length=30,
+#         blank=True)
+#     date_joined = models.DateTimeField(
+#         auto_now_add=True)
+#     updated_on = models.DateTimeField(
+#         auto_now=True)
+#     is_organizer=models.BooleanField(
+#         default=False)
+#
+#     def __str__(self):
+#         return self.user.username
