1e9fbfde5a22760b889aa643070ea8d44e8047c7
art <bobrolevv@yandex.ru>
2022-02-10 10:12:22 +0700

    pep8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,10 +2,9 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
-    # code = 200
-
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if obj.author == request.user:
+        if (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        ):
             return True
