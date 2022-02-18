47d2c61a6de7e6fd7dc971e7dcc94e24d12286b1
art <bobrolevv@yandex.ru>
2022-02-18 16:04:20 +0700

    fix workflow pep

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY',  get_random_secret_key())
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.environ.get("DEBUG", default=False)
 
