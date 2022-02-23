9ff2ea4f6177e045082f4c4ef5dc25e1aa50a9c6
art <bobrolevv@yandex.ru>
2022-02-23 13:39:52 +0700

    deploy 5

---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,49 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient, Recipe, User, Follow
+
+
+class Command(BaseCommand):
+    help = 'Load ingredient data to DB'
+
+    def handle(self, *args, **options):
+
+        with open('./recipes/data/ingredients.csv', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name, unit = row
+                Ingredient.objects.get_or_create(name=name,
+                                                 measurement_unit=unit)
+            print(f'add {Ingredient.objects.count()} ingredients')
+
+        for i in range(User.objects.count(), User.objects.count() + 10):
+            User.objects.get_or_create(
+                username=f'user_{i}',
+                first_name=f'first_{i}',
+                last_name=f'last_{i}',
+                email=f'user_{i}@m.ru',
+                password='user123456789',
+            )
+        print(f'add {i} users')
+
+        for i in range(9):
+            author = User.objects.get(id=f'{i+1}')
+            Recipe.objects.get_or_create(
+                name=f'Простой рецепт{i}',
+                text=f'Описание этого рецепта {i}',
+                image='/img/recipes/123.jpg',
+                cooking_time=i,
+                author=author
+            )
+        print(f'add {i} recipes')
+
+        user = User.objects.get(id='1')
+        for i in range(5):
+            author = User.objects.get(id=f'{i+1}')
+            Follow.objects.get_or_create(
+                user=user,
+                author=author
+            )
+        print(f'add {i} subsriptions')
